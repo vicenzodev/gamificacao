9ae1d7df897949b4881a49a8c6081800e6596a85
Erik Gomes <gomeserik35@gmail.com>
2025-04-12 11:15:00 -0300

    header position

--- src/pages/header.php
+++ src/pages/header.php
@@ -1,3 +1,15 @@
 <header>
-	
+	<div>
+        <input type="checkbox" id="check">
+            <label for="check" class="header-icons">
+                <i class='bx bx-menu' id="menu-icon"></i>
+                <i class='bx bx-x' id="close-icon"></i>
+            </label>
+        <div class="nome_app">
+
+        </div>
+        <div class="perfil">
+
+        </div>
+    </div>
 </header>
