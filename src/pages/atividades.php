179480fa82977e3b09778fa03e6928e1b41fb1d1
vicenzodev <heitor@vicenzo.dev>
2025-04-12 12:51:18 -0300

    alterações finais

--- src/pages/atividades.php
+++ src/pages/atividades.php
@@ -4,7 +4,7 @@
 <div class="container">
 	<div class="map">
 		<?php foreach($atv as $key => $content){?>
-			<a href="">
+			<a href="fazer_atividade?atv=<?php echo $content['id'];?>">
 				<div class="atv">
 					<h2 class="titulo"><?php echo $content['nome']?></h2>
 				</div><!--atv-->
--- src/pages/criar_atividade.php
+++ src/pages/criar_atividade.php
@@ -1,10 +1,24 @@
+<?php
+	if(isset($_POST['acao'])){
+		$atv = new Atividades($_POST['nome'],$_POST['qntQuest'],$_POST['xp']);
+	}
+?>
 <div class="container">
 	<form method="post">
-		<label>Nome: </label>
-		<input type="text" name="nome">
-		<label>Quantidade de questões: </label>
-		<input type="number" name="qntQuest" min="1" max="99">
-		<label>Quantidade de XP: </label>
-		<input type="number" name="xp" min="0" max="100000">
+		<div class="form-container">
+			<label>Nome: </label>
+			<input type="text" name="nome" required>
+		</div>
+		<div class="form-container">
+			<label>Quantidade de questões: </label>
+			<input type="number" name="qntQuest" min="1" max="99" required>
+		</div>
+		<div class="form-container">
+			<label>Quantidade de XP: </label>
+			<input type="number" name="xp" min="0" max="100000" required>
+		</div>
+		<div class="form-container">
+			<input type="submit" name="acao">
+		</div>
 	</form>
 </div>
