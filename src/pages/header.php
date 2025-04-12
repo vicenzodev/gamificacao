15e5a06b2f29d1e8007461fd587fc221eaedc2d5
Erik Gomes <gomeserik35@gmail.com>
2025-04-12 11:45:18 -0300

    alterações header

--- src/pages/header.php
+++ src/pages/header.php
@@ -6,10 +6,12 @@
                 <i class='bx bx-x' id="close-icon"></i>
             </label>
         <div class="nome_app">
-
+            <h1>Gamificação</h1>
         </div>
         <div class="perfil">
-
+            <div class="xp_bar_container">
+                <div class="xp_bar"></div>
+            </div>
         </div>
     </div>
 </header>
