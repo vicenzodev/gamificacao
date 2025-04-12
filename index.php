ca974eab840c4f01c2a30cf04c4572f9837d6c4d
vicenzodev <heitor@vicenzo.dev>
2025-04-12 10:27:15 -0300

    alterações iniciais

--- index.php
+++ index.php
@@ -4,8 +4,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Gamificação</title>
+	<link rel="stylesheet" type="text/css" href="sytle/style.css">
 </head>
 <body>
-	
+	<header>
+	</header>
+	<div class="container">
+		<div class="map">
+			<div class="atv">
+				<a href="/atividade1"><h2 class="titulo">Atividade 1</h2></a>
+			</div><!--atv-->
+		</div><!--map-->
+	</div><!--container-->
 </body>
 </html>
