2fa71cbe39bb4ca56d1df315cf3233c9818e112a
vicenzodev <heitor@vicenzo.dev>
2025-04-12 11:43:51 -0300

    criação de classes para manter atividades/questões/alternativas/

--- src/pages/atividades.php
+++ src/pages/atividades.php
@@ -1,7 +1,14 @@
+<?php
+	$atv = Atividades::getAtv();
+?>
 <div class="container">
 	<div class="map">
-		<div class="atv">
-			<a href="/atividade1"><h2 class="titulo">Atividade 1</h2></a>
-		</div><!--atv-->
+		<?php foreach($atv as $key => $content){?>
+			<a href="">
+				<div class="atv">
+					<h2 class="titulo"><?php echo $content['nome']?></h2>
+				</div><!--atv-->
+			</a>
+		<?php }?>
 	</div><!--map-->
 </div><!--container-->
